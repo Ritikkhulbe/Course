9fadc05606ecbf16cada79910a77b29612c06b39
Ritik <91615440+Ritikkhulbe@users.noreply.github.com>
2024-02-03 09:24:45 +0530

    DSA: Assignment 14

---
+++ DSA/as-14/as-14-1.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+using namespace std;
+enum Color { RED, BLACK };
+
+struct Node {
+    int data;
+    Color color;
+    Node* parent;
+    Node* left;
+    Node* right;
+
+    Node(int val) : data(val), color(RED), parent(nullptr), left(nullptr), right(nullptr) {}
+};
+
+class RedBlackTree {
+private:
+    Node* root;
+
+    void leftRotate(Node* x) {
+        Node* y = x->right;
+        x->right = y->left;
+
+        if (y->left != nullptr) {
+            y->left->parent = x;
+        }
+
+        y->parent = x->parent;
+
+        if (x->parent == nullptr) {
+            root = y;
+        } else if (x == x->parent->left) {
+            x->parent->left = y;
+        } else {
+            x->parent->right = y;
+        }
+
+        y->left = x;
+        x->parent = y;
+    }
+
+    void rightRotate(Node* y) {
+        Node* x = y->left;
+        y->left = x->right;
+
+        if (x->right != nullptr) {
+            x->right->parent = y;
+        }
+
+        x->parent = y->parent;
+
+        if (y->parent == nullptr) {
+            root = x;
+        } else if (y == y->parent->left) {
+            y->parent->left = x;
+        } else {
+            y->parent->right = x;
+        }
+
+        x->right = y;
+        y->parent = x;
+    }
+
+    void insertFixup(Node* z) {
+        while (z->parent != nullptr && z->parent->color == RED) {
+            if (z->parent == z->parent->parent->left) {
+                Node* y = z->parent->parent->right;
+                if (y != nullptr && y->color == RED) {
+                    z->parent->color = BLACK;
+                    y->color = BLACK;
+                    z->parent->parent->color = RED;
+                    z = z->parent->parent;
+                } else {
+                    if (z == z->parent->right) {
+                        z = z->parent;
+                        leftRotate(z);
+                    }
+
+                    z->parent->color = BLACK;
+                    z->parent->parent->color = RED;
+                    rightRotate(z->parent->parent);
+                }
+            } else {
+                Node* y = z->parent->parent->left;
+                if (y != nullptr && y->color == RED) {
+                    z->parent->color = BLACK;
+                    y->color = BLACK;
+                    z->parent->parent->color = RED;
+                    z = z->parent->parent;
+                } else {
+                    if (z == z->parent->left) {
+                        z = z->parent;
+                        rightRotate(z);
+                    }
+
+                    z->parent->color = BLACK;
+                    z->parent->parent->color = RED;
+                    leftRotate(z->parent->parent);
+                }
+            }
+        }
+
+        root->color = BLACK;
+    }
+
+
+public:
+    RedBlackTree() : root(nullptr) {}
+
+
+
+    void insert(int value) {
+        Node* z = new Node(value);
+        Node* y = nullptr;
+        Node* x = root;
+
+        while (x != nullptr) {
+            y = x;
+            if (z->data < x->data) {
+                x = x->left;
+            } else {
+                x = x->right;
+            }
+        }
+
+        z->parent = y;
+
+        if (y == nullptr) {
+            root = z;
+        } else if (z->data < y->data) {
+            y->left = z;
+        } else {
+            y->right = z;
+        }
+
+        insertFixup(z);
+    }
+    void preorderTraversal(Node* node) {
+        if (node != nullptr) {
+            cout << node->data <<"-";
+            if(!node->color)
+                cout<< "RED ";
+            else
+                cout<< "BLACK ";
+            preorderTraversal(node->left);
+            preorderTraversal(node->right);
+        }
+    }
+
+    void preorderTraversal() {
+        preorderTraversal(root);
+    }
+
+};
+
+int main() {
+    RedBlackTree rbTree;
+
+    rbTree.insert(5);
+    rbTree.insert(33);
+    rbTree.insert(21);
+    rbTree.insert(23);
+
+    rbTree.preorderTraversal();
+    return 0;
+}
